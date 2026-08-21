866cd2faf20fe8bcc5535ed1076cf0d0189f6702
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-21 13:24:30 +0300

    convert config to lua

---
+++ config/project.lua
@@ -0,0 +1,11 @@
+-- project definitions
+NAME = "demos-cloud-aws"
+DESCRIPTION_SHORT = "Demos for the AWS cloud"
+KEYWORDS = {
+    "aws",
+    "cloud",
+    "ec2",
+    "lambda",
+    "s3",
+    "terraform",
+}
